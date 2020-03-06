91c77c719dd288e37dc8f7b1dd0d9f420885a000
cosmicomic <liang.cjs@gmail.com>
2020-03-06 00:57:48 -0800

    can imitate paragraph flow using clustering and probabilities

--- prepare_text.py
+++ prepare_text.py
@@ -3,26 +3,38 @@ import random
 import string
 import numpy as np
 
+from collections import Counter
+
 from scipy import sparse
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 from joblib import dump, load
+import markovify
 
-def draw_from_cluster(n, cluster, samples):
+def draw_from_cluster(n, cluster, samples, sentences=False):
     for _ in range(n):
         cluster_index = random.randrange(0, len(cluster))
         sentence_index = cluster[cluster_index]
-        print(' '.join(samples[sentence_index]) + '.')
+        if not sentences:
+            print(' '.join(samples[sentence_index]) + '.')
+        else:
+            printed_sentence = samples[sentence_index].replace("\n", " ")
+            print(printed_sentence)
 
-def prepare_text():
-    with open("Corpus/Northanger.txt", encoding="utf8") as f:
-        northanger_text = f.read()
+def prepare_text(title_string):
+    with open("Corpus/" + title_string + ".txt", encoding="utf8") as f:
+        text = f.read()
 
     # remove chapter headings
-    new_text = northanger_text
-    matches = re.findall("CHAPTER \d+|Chapter \d+", northanger_text)
+    new_text = text
+    matches = re.findall("CHAPTER \w+|Chapter \w+", text)
     for match in matches:
         new_text = re.sub(match, '', new_text)
+
+    # remove periods from end of titles
+    new_text = new_text.replace("Mrs.", "Mrs")
+    new_text = new_text.replace("Mr.", "Mr")
+    new_text = new_text.replace("Dr.", "Dr")
 
     return new_text
 
@@ -57,9 +69,9 @@ def prepare_samples(new_text):
         tokenized_sentence = re.split(r'\W+', sentence)
         samples.append(tokenized_sentence)
 
-    return samples
+    return sentences, samples
 
-def cluster(k, ngrams, samples):
+def cluster(k, ngrams, samples, title_string):
     # represent as sparse vectors
     n_features = len(ngrams)
     n_samples = len(samples)
@@ -76,28 +88,122 @@ def cluster(k, ngrams, samples):
 
     print("Clustering")
     # cluster
-    kmeans = KMeans(n_clusters=4).fit(X)
-    dump(kmeans, 'kmeans.joblib') 
+    kmeans = KMeans(n_clusters=k).fit(X)
+    dump(kmeans, 'kmeans_' + title_string + '.joblib') 
     print(kmeans.labels_)
 
     labels = kmeans.labels_
     unique, counts = np.unique(labels, return_counts=True)
     print(dict(zip(unique, counts)))
 
-new_text = prepare_text()
-samples = prepare_samples(new_text)
+    return kmeans
 
-kmeans = load('kmeans.joblib')
+def show_cluster_sentences(n, sentences, labels):
+    cluster_dict = {}
+    for i in range(len(sentences)):
+        if labels[i] not in cluster_dict:
+            cluster_dict[labels[i]] = [i]
+        else:
+            cluster_dict[labels[i]].append(i)
+
+    for key in cluster_dict.keys():
+        print("Cluster", key)
+        draw_from_cluster(5, cluster_dict[key], sentences, True)
+        print("\n")
+
+def sort_samples_by_cluster(labels, samples):
+    cluster_dict = {}
+    for i in range(len(samples)):
+        if labels[i] not in cluster_dict:
+            cluster_dict[labels[i]] = [samples[i]]
+        else:
+            cluster_dict[labels[i]].append(samples[i])
+
+    return cluster_dict
+
+def get_bags_of_words(cluster_dict):
+    bag_dict = {}
+    for cluster_index in cluster_dict.keys():
+        bag_of_words = []
+        sentences = cluster_dict[cluster_index]
+        for sentence in sentences:
+            bag_of_words += re.split(r'\W+', sentence)
+        bag_dict[cluster_index] = bag_of_words
+    return bag_dict
+
+
+k = 5
+title_string = "Persuasion"
+new_text = prepare_text(title_string)
+sentences, samples = prepare_samples(new_text)
+unigrams = get_unigrams(new_text)
+# kmeans = cluster(5, unigrams, samples, title_string)
+kmeans = load('kmeans_persuasion.joblib')
 labels = kmeans.labels_
+unique, counts = np.unique(labels, return_counts=True)
+cluster_sizes = dict(zip(unique, counts))
 
-cluster_dict = {}
-for i in range(len(samples)):
-    if labels[i] not in cluster_dict:
-        cluster_dict[labels[i]] = [i]
-    else:
-        cluster_dict[labels[i]].append(i)
+transitions = np.zeros((k, k))
+for i in range(1, len(samples)):
+    curr = labels[i]
+    prev = labels[i - 1]
+    transitions[prev][curr] += 1
+
+transitions_normalized = np.array([transitions[0] / cluster_sizes[0]])
+for i in range(1, k):
+    add_row = np.array([transitions[i] / cluster_sizes[i]])
+    transitions_normalized = np.concatenate((transitions_normalized, add_row))
+
+transitions_list = transitions_normalized.tolist()
+print(transitions_list)
+
+gen_sequence = [4]
+prev = gen_sequence[0]
+for i in range(1, 8):
+    print("prev", prev)
+    next = random.choices(list(range(k)), weights=transitions_list[prev])[0]
+    gen_sequence.append(next)
+    prev = next
+
+print(gen_sequence)
+
+cluster_dict = sort_samples_by_cluster(labels, sentences)
+markov_chains = []
 
 for key in cluster_dict.keys():
-    print(key)
-    draw_from_cluster(10, cluster_dict[key], samples)
-    print("\n")
+    cluster_continuous = '. '.join(cluster_dict[key])
+    text_model = markovify.Text(cluster_continuous, state_size=2)
+    markov_chains.append(text_model)
+
+sentence_sequence = []
+
+for state in gen_sequence:
+    state_model = markov_chains[state]
+    sentence_sequence.append(text_model.make_sentence())
+
+print(" ".join(sentence_sequence))
+
+
+
+# show_cluster_sentences(10, sentences, labels)
+
+# cluster_dict = sort_samples_by_cluster(labels, sentences)
+
+# for key in cluster_dict.keys():
+#     cluster_continuous = '. '.join(cluster_dict[key])
+#     text_model = markovify.Text(cluster_continuous, state_size=2)
+
+#     print("CLUSTER", key)
+#     for i in range(5):
+#         print(text_model.make_sentence())
+# bag_dict = get_bags_of_words(cluster_dict)
+
+# c = Counter(bag_dict[0])
+# frequent1 = [word for (word, _) in c.most_common(50)]
+# print(frequent1)
+
+# c = Counter(bag_dict[1])
+# frequent2 = [word for (word, _) in c.most_common(50)]
+# print(frequent2)
+
+# print(set(frequent1).intersection(set(frequent2)))
--- try.py
+++ try.py
@@ -1,10 +1,10 @@
 import markovify
 import re
-import markov_novel
-import spacy
+# import markov_novel
+# import spacy
 import nltk
 
-nlp = spacy.load('en_core_web_sm')
+# nlp = spacy.load('en_core_web_sm')
 
 class NLTKPOSifiedText(markovify.Text):
     def word_split(self, sentence):
@@ -19,13 +19,13 @@ class NLTKPOSifiedText(markovify.Text):
         sentence = " ".join(word.split("::")[0] for word in words)
         return sentence
 
-class SpacyPOSifiedText(markovify.Text):
-    def word_split(self, sentence):
-        return ["::".join((word.orth_, word.pos_)) for word in nlp(sentence)]
+# class SpacyPOSifiedText(markovify.Text):
+#     def word_split(self, sentence):
+#         return ["::".join((word.orth_, word.pos_)) for word in nlp(sentence)]
 
-    def word_join(self, words):
-        sentence = " ".join(word.split("::")[0] for word in words)
-        return sentence
+#     def word_join(self, words):
+#         sentence = " ".join(word.split("::")[0] for word in words)
+#         return sentence
 
 def process(text):
     # remove chapter headings
@@ -36,32 +36,37 @@ def process(text):
     return new_text
 
 # Get raw text as string.
-with open("Corpus/Northanger.txt", encoding="utf8") as f:
-    northanger_text = f.read()
+# with open("Corpus/Northanger.txt", encoding="utf8") as f:
+#     northanger_text = f.read()
 
-with open("Corpus/Pride.txt", encoding="utf8") as f:
-    pride_text = f.read()
+# with open("Corpus/Pride.txt", encoding="utf8") as f:
+#     pride_text = f.read()
 
-with open("Corpus/Sense.txt", encoding="utf8") as f:
-    sense_text = f.read()
+# with open("Corpus/Sense.txt", encoding="utf8") as f:
+#     sense_text = f.read()
 
-combined_text = " ".join([northanger_text, pride_text, sense_text])
+# combined_text = " ".join([northanger_text, pride_text, sense_text])
 
 # combined_text = northanger_text
     
-processed_text = process(combined_text)
+with open("Corpus/Persuasion.txt", encoding="utf8") as f:
+    persuasion_text = f.read()
+processed_text = process(persuasion_text)
+# processed_text = process(combined_text)
 # Build the model.
-# text_model = markovify.Text(processed_text, state_size=3)
+text_model = markovify.Text(processed_text, state_size=2)
 # text_model = SpacyPOSifiedText(processed_text)
-text_model = NLTKPOSifiedText(processed_text, state_size=3)
+# text_model = NLTKPOSifiedText(processed_text, state_size=3)
 
 # novel = markov_novel.Novel(text_model, chapter_count=5)
 # novel.write(novel_title='my-novel', filetype='md')
-
+sentence_sequence = []
 # Print five randomly-generated sentences
-for i in range(5):
-    print(text_model.make_sentence())
+for i in range(8):
+    sentence_sequence.append(text_model.make_short_sentence(280))
+
+print(" ".join(sentence_sequence))
 
 # Print three randomly-generated sentences of no more than 280 characters
-for i in range(3):
-    print(text_model.make_short_sentence(280))
+# for i in range(3):
+#     print(text_model.make_short_sentence(280))
